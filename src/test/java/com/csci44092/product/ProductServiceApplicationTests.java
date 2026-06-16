aa66b4beeb4f31cb7f326fffc6e5e9e3685dfd0f
dev-rajitha <rkrishan894@gmail.com>
2026-06-16 18:47:29 +0530

    feat: initialize product-service project with Maven wrapper and Spring Boot scaffold

---
+++ src/test/java/com/csci44092/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csci44092.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
